46f98b50ff08173bd8408545904df4427d1c5c89
danonnn23 <tolochkodan.work@gmail.com>
2026-01-11 15:58:19 +0200

    Changed styles for big screens (1440px and more)

--- todo/src/components/icons/CheckIcon.jsx
+++ todo/src/components/icons/CheckIcon.jsx
@@ -1,6 +1,6 @@
 function CheckIcon() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--checkbox-fg-clr)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--checkbox-fg-clr)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg>
     ) 
 }
 
